f913922d8e52fba1b5598a44682d1ed3897d35cc
Wojtek Gustowski <wgustowski@gmail.com>
2019-11-14 19:57:22 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 VERSION="6.2.4"
 sudo yum update -y
+sudo yum install yum-plugin-versionlock -y
 echo 'Installing java'
-sudo yum install java-1.8.0-openjdk unzip git htop -y
+sudo yum install java-1.8.0-openjdk unzip git htop wget nano -y
 echo 'Installing Elasticsearch'
 sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 sudo tee /etc/yum.repos.d/elasticsearch.repo <<EOF
@@ -16,24 +17,24 @@ autorefresh=1
 type=rpm-md
 EOF
 sudo yum install elasticsearch-$VERSION -y
+sudo yum versionlock elasticsearch-$VERSION
 cp /vagrant/config/elasticsearch/elasticsearch.yml /etc/elasticsearch/
-#sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
 sudo /bin/systemctl start elasticsearch.service
 
 echo 'Installing Kibana'
 sudo yum install kibana-$VERSION -y
+sudo yum versionlock kibana-$VERSION
 sudo systemctl enable kibana
 sudo systemctl start kibana
 cp /vagrant/config/kibana/kibana.yml /etc/kibana/
 
-#sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable kibana.service
 sudo systemctl start kibana.service
 
 echo 'Installing Logstash'
 sudo yum install logstash-$VERSION -y
-
+sudo yum versionlock logstash-$VERSION
 cp  /vagrant/config/logstash/* /etc/logstash/conf.d/
 
 sudo -u logstash /usr/share/logstash/bin/logstash --path.settings /etc/logstash -t
